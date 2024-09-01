400c72508e731e363d31dbb60d3db169687abf8b
Akshay Gandhi <akshaygandhi03@gmail.com>
2024-09-01 23:37:51 +0530

    Example 12 : Get  a lis f even number an then sum it all in one line

--- src/main/java/com/example/project/ProjectApplication.java
+++ src/main/java/com/example/project/ProjectApplication.java
@@ -21,18 +21,18 @@ public class ProjectApplication {
 		SpringApplication.run(ProjectApplication.class, args);
 		var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 		Programs vehicle = context.getBean(Programs.class);
-		List<Integer>  evenNumbers = vehicle.getExampleServices().getEvenNo();
+		List<Integer> evenNumbers = vehicle.getExampleServices().getEvenNo();
 		System.out.println("The Even Number is " + evenNumbers);
-		List <Employee>  empDeatils = vehicle.getExampleServices().getEmpDetails();
+		List<Employee> empDeatils = vehicle.getExampleServices().getEmpDetails();
 		System.out.println("The Employees  is " + empDeatils);
-		List<Integer> number =  Arrays.asList(10,20,30);
+		List<Integer> number = Arrays.asList(10, 20, 30);
 
-		int sumNumber1 = number.stream().reduce(1,(i, j) -> i + j);
-        int multiplyNumber1 = number.stream().reduce(1,(i, j) -> i * j);
+		int sumNumber1 = number.stream().reduce(1, (i, j) -> i + j);
+		int multiplyNumber1 = number.stream().reduce(1, (i, j) -> i * j);
 
 		List<Integer> sumNumber = Collections.singletonList(number.stream().mapToInt(i -> i).sum());
 		System.out.println(sumNumber1);
-        System.out.println(multiplyNumber1);
+		System.out.println(multiplyNumber1);
 
 		int minNo = number.stream().min(Integer::compareTo).orElse(0);
 		System.out.println("Min vale" + minNo);
@@ -53,38 +53,42 @@ public class ProjectApplication {
 
 		//Convert a string list to integer list and parse it to integer
 
-		List<String> sringNumbers = Arrays.asList("2","8","89","23","67","90","55");
+		List<String> sringNumbers = Arrays.asList("2", "8", "89", "23", "67", "90", "55");
 		List<Integer> newSnumber = sringNumbers.stream().map(Integer::parseInt).collect(Collectors.toList());
 		System.out.println(newSnumber);
 
-		List<String> numberNull =  Arrays.asList("10","20",null,"30",null,"");
+		List<String> numberNull = Arrays.asList("10", "20", null, "30", null, "");
 
 		List<String> numList = numberNull.stream().filter(Objects::nonNull).filter(str -> !str.trim().isEmpty()).collect(Collectors.toList());
 		System.out.println(numList);
 
-		List<String> stringList = Arrays.asList("Tata","Consultancy","Services","Metlife");
+		List<String> stringList = Arrays.asList("Tata", "Consultancy", "Services", "Metlife");
 
 		String lists = stringList.stream().collect(Collectors.joining(","));
 		System.out.println(lists);
 
-        long countoString = Arrays.stream(lists.split(",")).count();
-        System.out.println(countoString);
+		long countoString = Arrays.stream(lists.split(",")).count();
+		System.out.println(countoString);
 
 		String value = "akshay";
 		List<String> values = Arrays.stream(value.split("")).collect(Collectors.toList());
 		System.out.println(values);
 
-        List<Character> valuess = value.chars().mapToObj(c ->(char) c).collect(Collectors.toList());
-        System.out.println(valuess);
+		List<Character> valuess = value.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
+		System.out.println(valuess);
 
-        Map<Character, Long> valuesss = value.chars().mapToObj(c ->(char) c).collect(Collectors.groupingBy(c -> c,Collectors.counting()));
+		Map<Character, Long> valuesss = value.chars().mapToObj(c -> (char) c).collect(Collectors.groupingBy(c -> c, Collectors.counting()));
 
 
-        Optional<Integer> thirdHigh  = number.stream().sorted((a, b) -> b-a).skip(2).findFirst();
-        System.out.println(thirdHigh);
+		Optional<Integer> thirdHigh = number.stream().sorted((a, b) -> b - a).skip(2).findFirst();
+		System.out.println(thirdHigh);
 
-        
-        
 
-    }
+		List<Integer> setOfnum = Arrays.asList(10, 6, 8, 90, 4, 78, 9, 7, 88, 45, 46, 34, 39);
+
+		List<Integer> setofEven = setOfnum.stream().filter(e -> e % 2 == 0).collect(Collectors.toList());
+		System.out.println(setofEven);
+		int EvenSum = setOfnum.stream().filter(e -> e % 2 == 0).reduce(0, (e, j) -> e + j);
+		System.out.println(EvenSum);
+	}
 }
